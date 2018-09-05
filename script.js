1bb0461a7e268f63d60b731484491af741d47005
Alirj18 <ali@youbiu.com>
2018-09-05 15:17:02 -0700

    Invoke CSS3 Animation with jQuery
    A simple jQuery function to invoke css3 animations.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {
+    $(".didScroll").each(function(){
+      var pos = $(this).offset().top;
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slideIn");
+        }
+    });
+  });
